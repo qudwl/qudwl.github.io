2529a2a0086b32e96a8f463bd9356f0897d1728d
Byeongji Lim <byeongjilim@gmail.com>
2025-02-12 15:05:38 +0000

    feat: update header menu design

--- src/components/Header/components/HeaderMenu/HeaderMenu.tsx
+++ src/components/Header/components/HeaderMenu/HeaderMenu.tsx
@@ -1,9 +1,16 @@
+import styles from './HeaderMenu.module.scss'
+
 const HeaderMenu = () => {
-    return <nav>
-        <button>Home</button>
-        <button>Projects</button>
-        <button>About</button>
-        <button>Settings</button>
+
+    const items = [
+        { label: 'Home', selected: true },
+        { label: 'Projects', selected: false },
+        { label: 'About', selected: false },
+        { label: 'Settings', selected: false },
+    ]
+
+    return <nav role="tablist">
+        {items.map((item, index) => <button className={styles.tab} key={index} aria-selected={item.selected} role="tab">{item.label}</button>)}
     </nav>
 }
 
