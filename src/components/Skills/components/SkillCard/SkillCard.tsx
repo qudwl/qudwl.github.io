5da954aa8be1ca6770d14bf645ac38fd664ac0ba
Byeongji Lim <byeongjilim@gmail.com>
2025-09-06 12:40:42 +0200

    feat: add skills section

---
+++ src/components/Skills/components/SkillCard/SkillCard.tsx
@@ -0,0 +1,17 @@
+import type { SkillCardProps } from "./types";
+import styles from "./SkillCard.module.scss";
+
+const SkillCard: React.FC<SkillCardProps> = ({ name, level, Icon }) => {
+    const levels = ["Beginner", "Intermediate", "Advanced"];
+    return (
+        <div className={styles.skillCard}>
+            <Icon size={"2em"} />
+            <div>
+                <h3>{name}</h3>
+                <p>{levels[level]}</p>
+            </div>
+        </div>
+    );
+}
+
+export default SkillCard;
